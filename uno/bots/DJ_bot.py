3e86ad4e70bc6556900d8db6e786ef263317d902
Nikita <nikitakinztel45@gmail.com>
2025-12-03 02:11:54 +0700

    new version 2.0

--- uno/bots/DJ_bot.py
+++ uno/bots/DJ_bot.py
@@ -1,64 +1,103 @@
 from __future__ import annotations
-
 from random import choice
 from typing import List, Optional
 
-from ..engine.card import Card, CardColor
+from ..engine.card import Card, CardColor, CardLabel
 from ..player.player import Player, PlayerAction
 
+ #БИБИБИБЛИОТЕКААА!!! ^^^^^^^^^^^
 
-class DragonJump(Player):
-    """
-    A bot that plays its wild cards first.
-    """
-
+class DragonJump(Player): # <<<<<<<< НАШ КЛАССИКК
     def __init__(self, name: str, player_id: int):
         super().__init__(name, player_id)
         self._top_card: Optional[Card] = None
         self._current_color: Optional[CardColor] = None
 
     def update_game_state(
-        self, playable_cards: List[Card], top_card: Card, current_color: CardColor
+        self, playable_cards: List[Card], topcard: Card, currentcolor: CardColor
     ) -> None:
-        """
-        Update bot's knowledge of current game state
-        """
-        self._top_card = top_card
-        self._current_color = current_color
+        self._topcard = topcard
+        self._current_color = currentcolor
 
-    def choose_action(self) -> PlayerAction:
-        """
-        Returns a valid card or draw action. Favors wild cards first.
-        """
-        # Create a list of valid selections with (index, card) tuples
+    def choose_action(self) -> PlayerAction: # тутс реализована основная тактикаа игрыы
+        
         valid_selections = []
         for index, card in enumerate(self.hand):
-            if card.can_play_on(self._top_card, self._current_color):
+            if card.can_play_on(self._topcard, self._current_color):
                 valid_selections.append((index, card))
-
+        draw_cards = [
+            sel for sel in valid_selections
+            if sel[1].label in (CardLabel.DRAW_TWO, CardLabel.WILD_DRAW_FOUR)
+        ]
+        if draw_cards:
+            card = draw_cards[0][1]
+            new_color = self.choose_color(card) if card.is_wild else None
+            return self.play_card(card, new_color)
+        action_cards = [
+            sel for sel in valid_selections
+            if sel[1].label in (CardLabel.SKIP, CardLabel.REVERSE)
+        ]
+        if action_cards:
+            card = action_cards[0][1]
+            new_color = self.choose_color(card) if card.is_wild else None
+            return self.play_card(card, new_color)
+        wilds = [
+            sel for sel in valid_selections
+            if sel[1].is_wild and sel[1].label != CardLabel.WILD_DRAW_FOUR
+        ]
+        if wilds:
+            card = wilds[0][1]
+            new_color = self.choose_color(card)
+            return self.play_card(card, new_color)
+        number_cards = [
+            sel for sel in valid_selections
+            if not sel[1].is_wild
+            and sel[1].label not in (
+                CardLabel.DRAW_TWO,
+                CardLabel.SKIP,
+                CardLabel.REVERSE
+            )
+        ]
+        if number_cards:
+            try:
+                number_cards.sort(key=lambda x: x[1].value)
+                card = number_cards[0][1]
+                return self.play_card(card)
+            except (AttributeError, ValueError):
+                pass
         if valid_selections:
-            # Find wild cards first
             wilds = [
                 selection for selection in valid_selections if selection[1].is_wild
             ]
 
             if wilds:
-                # Play the first wild card found
                 index, card = wilds[0]
                 new_color = self.choose_color(card) if card.is_wild else None
                 return self.play_card(card, new_color)
             else:
-                # Play a random non-wild card
-                index, card = choice(valid_selections)
-                return self.play_card(card)
+                color_groups = {
+                    CardColor.RED: [],
+                    CardColor.BLUE: [],
+                    CardColor.GREEN: [],
+                    CardColor.YELLOW: []
+                }
+                for card in self.hand:
+                    if card.color in color_groups:
+                        color_groups[card.color].append(card)
+                dominant_color = max(
+                    color_groups.keys(),
+                    key=lambda color: len(color_groups[color])
+                )
+                dominant_cards = color_groups[dominant_color]
+                best_card = max(
+                    dominant_cards,
+                    key=lambda card: card.label.value
+                )
+            return self.play_card(best_card)
         else:
-            # No playable cards, must draw
             return PlayerAction(draw_card=True)
 
-    def choose_color(self, wild_card: Card) -> CardColor:
-        """
-        Returns an advantageous color for a wild card.
-        """
+    def choose_color(self, wildcard: Card) -> CardColor: # тутсс мы выбираемм цвеет!
         color_counts = {
             color: 0
             for color in [
@@ -68,23 +107,14 @@ class DragonJump(Player):
                 CardColor.YELLOW,
             ]
         }
-
-        # Count non-wild cards by color
         for card in self.hand:
-            if not card.is_wild:  # card.color != CardColor.WILD
+            if not card.is_wild:
                 color_counts[card.color] += 1
-
-        # Return the color with the most cards
         return max(color_counts, key=color_counts.get)
 
-    def decide_say_uno(self) -> bool:
-        """
-        Always says UNO when applicable.
-        """
+    def decide_say_uno(self) -> bool: #туттсс мы говоримм унооо!
         return True
 
-    def should_play_drawn_card(self, drawn_card: Card) -> bool:
-        """
-        Always plays drawn card if possible.
-        """
-        return drawn_card.can_play_on(self._top_card, self._current_color)
+    def should_play_drawn_card(self, drawncard: Card) -> bool: # проверочкаа можно ли сыграть новой картоойй!
+        return drawncard.can_play_on(self._topcard, self._current_color)
+# <3
